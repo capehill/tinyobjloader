afdd3fa785ac556d12e2e0d99f3bbf6239ab5f31
Paul Melnikow <github@paulmelnikow.com>
2026-02-23 16:15:52 -0500

    Test Python + NumPy in CI (#405)
    Add test tasks to GitHub Actions which test the generated wheels using different Python and NumPy versions.
    
    I've added four tests for issues #400 and they are all failing in the way I expect – surfacing disparities between `num_face_vertices` and `numpy_num_face_vertices()`. I've also added tests for issue #401 which is passing, and #402 is also covered in the #400 tests so those will need further investigation at a later time.
    
    Closes #403

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 # Adapted from https://github.com/pybind/python_example/blob/master/setup.py
 import sys
 
-#from pybind11 import get_cmake_dir
+# from pybind11 import get_cmake_dir
 # Available at setup time due to pyproject.toml
-from pybind11.setup_helpers import Pybind11Extension#, build_ext
+from pybind11.setup_helpers import Pybind11Extension  # , build_ext
 from setuptools import setup
 
 try:
@@ -11,9 +11,6 @@ try:
     from .python import _version
 except:
     __version__ = "2.0.0rc10"
-
-with open("README.md", "r", encoding="utf8") as fh:
-    long_description = fh.read()
 
 # The main interface is through Pybind11Extension.
 # * You can add cxx_std=11/14/17, and then build_ext can be removed.
@@ -25,27 +22,27 @@ with open("README.md", "r", encoding="utf8") as fh:
 #   reproducible builds (https://github.com/pybind/python_example/pull/53)
 
 ext_modules = [
-    Pybind11Extension("tinyobjloader",
+    Pybind11Extension(
+        "tinyobjloader",
         sorted(["python/bindings.cc", "python/tiny_obj_loader.cc"]),
         # Example: passing in the version to the compiled code
-        define_macros = [('VERSION_INFO', __version__)],
+        define_macros=[("VERSION_INFO", __version__)],
         cxx_std=11,
-        ),
+    ),
 ]
 
 setup(
     name="tinyobjloader",
-    packages=['python'],
-    #version=__version__,
+    packages=["python"],
+    # version=__version__,
     author="Syoyo Fujita",
     author_email="syoyo@lighttransport.com",
     url="https://github.com/tinyobjloader/tinyobjloader",
-    #project_urls={
+    # project_urls={
     #    "Issue Tracker": "https://github.com/tinyobjloader/tinyobjloader/issues",
-    #},
+    # },
     description="Tiny but powerful Wavefront OBJ loader",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -60,10 +57,10 @@ setup(
         "Programming Language :: Python :: 3",
     ],
     ext_modules=ext_modules,
-    #extras_require={"test": "pytest"},
+    # extras_require={"test": "pytest"},
     # Currently, build_ext only provides an optional "highest supported C++
     # level" feature, but in the future it may provide more features.
     # cmdclass={"build_ext": build_ext},
-    #zip_safe=False,
-    #python_requires=">=3.6",
+    # zip_safe=False,
+    # python_requires=">=3.6",
 )
